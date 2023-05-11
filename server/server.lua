e5dae473875fa60b4acc7ad64a63e2f37b8c059d
KRS <131356071+KRS-KAROS@users.noreply.github.com>
2023-05-11 17:46:54 +0200

    Add files via upload

--- server/server.lua
+++ server/server.lua
@@ -3,29 +3,30 @@ ESX = exports["es_extended"]:getSharedObject()
 -- Funzione Ritira
 RegisterNetEvent('ritirare', function(importo)
     local xPlayer = ESX.GetPlayerFromId(source)
-    print('money')
     local sBanca = xPlayer.getAccount("bank").money
     local sContanti = xPlayer.getAccount("money").money
     if sBanca >= importo then
-        print('money')
         xPlayer.removeAccountMoney("bank", importo)
         xPlayer.addMoney(importo)
+        TriggerClientEvent('esx:showNotification', source, 'Hai ritirato $' .. importo .. ' dalla tua banca.')
+    else
+        TriggerClientEvent('esx:showNotification', source, 'Non hai abbastanza denaro nella tua banca.')
     end
 end)
 
--- Funzione Deposita 
+-- Funzione Deposita
 RegisterNetEvent('deposita', function(importo)
     local xPlayer = ESX.GetPlayerFromId(source)
-    print('money')
     local sBanca = xPlayer.getAccount("bank").money
     local sContanti = xPlayer.getAccount("money").money
     if sContanti >= importo then
-          
         xPlayer.removeAccountMoney("money", importo)
         xPlayer.addAccountMoney('bank', importo)
+        TriggerClientEvent('esx:showNotification', source, 'Hai depositato $' .. importo .. ' nella tua banca.')
+    else
+        TriggerClientEvent('esx:showNotification', source, 'Non hai abbastanza denaro in contanti.')
     end
 end)
-
 
 -- Funzione Deposita
 RegisterNetEvent("trasferisci", function(importo, target)
@@ -42,20 +43,18 @@ RegisterNetEvent("trasferisci", function(importo, target)
         xPlayer.removeAccountMoney("bank", importo)
         xTarget.addAccountMoney('bank', importo)
 
-        xPlayer.showNotification("Hai trasferito $"..importo.." a Id"..xTarget.source)
-        xPlayer.showNotification("Hai Ricevuto $"..importo.." da Id"..xPlayer.source)
+        xPlayer.showNotification("Hai trasferito $" .. importo .. " a ID " .. target)
+        xTarget.showNotification("Hai ricevuto $" .. importo .. " da ID " .. source)
+    else
+        xPlayer.showNotification("Non hai abbastanza denaro nella tua banca.")
     end
-
 end)
-
 
 -- Funzione Soldi in Banca
 ESX.RegisterServerCallback('soldi:getData', function(source, cb)
     local data = {}
     local xPlayer = ESX.GetPlayerFromId(source)
-
     data.bank = xPlayer.getAccount("bank").money
-
     cb(data)
 end)
 
